946572526d84bbf850a03b90ace781786abfdea5
igrad <iangradert@gmail.com>
2019-09-19 19:58:46 -0500

    Lots of progress on article and filter selection

--- js/posts.js
+++ js/posts.js
@@ -3,58 +3,111 @@
 var catalog = [
    {  id: 0,
       title: "Demo Post With a Very Long Title",
+      date: "09/18/2018",
+      month: 9,
+      year: 2018,
+      tags: ["misc"],
+      header: "Test post to format website"
+   },
+   {  id: 1,
+      title: "Second Post",
+      date: "09/17/2019",
+      month: 9,
+      year: 2019,
+      tags: ["misc"],
+      header: "Test post to format website"
+   },
+   {  id: 2,
+      title: "Recent Post",
+      date: "09/18/2019",
+      month: 9,
+      year: 2019,
+      tags: ["misc"],
+      header: "Test post to format website"
+   },
+   {  id: 3,
+      title: "Last Post",
       date: "09/18/2019",
       month: 9,
       year: 2019,
       tags: ["misc"],
       header: "Test post to format website"
    }
-
 ]
 
 
-function GetListOfPosts (filter) {
+function GetListOfPosts(filter) {
    // Return a list of references to the posts we want, as determined by filter
    if (filter == null) return catalog
    else return catalog.filter(item => item.tags.includes(filter))
 }
 
-function GetPostPreviews (filter) {
+function GetPostPreviews(filter) {
    // Get HTML code for what to display in a series of post previews
 
 }
 
-function ShowIndex (filter) {
+function ShowIndex(filter) {
    // Post HTML code of the index of the included posts to screen
    var list = GetListOfPosts(filter)
 
    var html = "<div id='post_index'><div id='fade'></div>"
+   html += "<div id='post_index_header' style='box-shadow: 0px 2px #dddddd; font-weight: bold'>Filtered Posts</div>"
 
    var currentYear = 0
    for (var i = list.length-1; i > -1; i--) {
       var item = list[i];
       if (item.year != currentYear) {
+         if (currentYear != 0) html += "<br/>"
          currentYear = item.year
          html += "<div id='year_" + currentYear + "' class='index_year'>"
          html += currentYear
          html += "</div>"
       }
-      html += "<div id='post_" + item.id + "' class='index_entry'>"
+      html += "<div id='" + item.id + "' class='index_entry'>"
       html += item.title
       html += "</div>"
    }
 
    html += "</div></div>"
 
-   $("#posts_index_wrapper")[0].innerHTML = html;
+   // Post the HTML to the page
+   $("#posts_index_wrapper")[0].innerHTML = html
+
+   // Update event handlers on necessary elements
+   $(".index_entry").hover(function() {
+      $(this).css("color", "white")
+   }, function () {
+      $(this).css("color", "")
+   })
+   $(".index_entry").click(function() {
+      ShowPost($(this).attr("id"))
+   })
 }
 
-function ShowPost (postID) {
-   //
+function ShowPost(postID) {
+   // Load HTML of post to the screen in iframe
+   $("#posts_body").attr("src", "posts\\" + postID + ".html")
 }
 
-function ShowPosts (filter) {
+function ShowMostRecentPost() {
+   // Displays the most recently-created post
+   ShowPost(catalog.length - 1)
+}
+
+function ShowPosts(filter) {
    // Accepts an enumeration as the filter to determine what type of
    // posts should be grabbed and loaded
 
+}
+
+function UpdateFilters () {
+   // Fetch the filter selection updates from screen and populate index
+   // results accordingly
+   var filter = null
+   if ($(".filter_cb[ischecked=true]").length > 0) {
+      filter = $(".filter_cb[ischecked=true]").attr("id").split("_")[1]
+   }
+
+   ShowIndex(filter)
 }
